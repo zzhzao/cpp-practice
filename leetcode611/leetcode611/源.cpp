637b6855bec0fdab5a1447d28d1b02f89b521fce
zzhZAO <3516422381@qq.com>
2025-06-25 06:03:44 +0800

    leetcode611 有效三角形的个数

---
+++ leetcode611/leetcode611/源.cpp
@@ -0,0 +1,26 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+class Solution {
+public:
+    int triangleNumber(vector<int>& nums) {
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        int ans = 0;
+        for (int i = 0; i < n; ++i) {
+            for (int j = i + 1; j < n; ++j) {
+                int left = j + 1, right = n - 1, k = j;
+                while (left <= right) {
+                    int mid = (left + right) / 2;
+                    if (nums[mid] < nums[i] + nums[j]) {
+                        k = mid;
+                        left = mid + 1;
+                    }
+                    else {
+                        right = mid - 1;
+                    }
+                }
+                ans += k - j;
+            }
+        }
+        return ans;
+    }
+};
