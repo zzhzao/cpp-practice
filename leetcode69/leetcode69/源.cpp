ec8b151e9f066f3ed13a2068faf946eb90cf841a
zzhZAO <3516422381@qq.com>
2025-07-30 22:37:55 +0800

    leetcode69x的平方根

---
+++ leetcode69/leetcode69/源.cpp
@@ -0,0 +1,12 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+class Solution {
+public:
+    int mySqrt(int x) {
+        if (x == 0) {
+            return 0;
+        }
+        int ans = exp(0.5 * log(x));
+        return ((long long)(ans + 1) * (ans + 1) <= x ? ans + 1 : ans);
+    }
+};
+
