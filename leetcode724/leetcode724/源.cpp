8d69e862d0457ae74eb9eb0001243e350fc9a9aa
zzhZAO <3516422381@qq.com>
2025-08-07 22:34:24 +0800

    寻找数组的中心下标

---
+++ leetcode724/leetcode724/源.cpp
@@ -0,0 +1,16 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+class Solution {
+public:
+    int pivotIndex(vector<int>& nums) {
+        int total = accumulate(nums.begin(), nums.end(), 0);
+        int sum = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (2 * sum + nums[i] == total) {
+                return i;
+            }
+            sum += nums[i];
+        }
+        return -1;
+    }
+};
+
