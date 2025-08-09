c4a13adeb54a96a09617fafe5b62207a310a4a59
zzhZAO <3516422381@qq.com>
2025-08-09 21:01:36 +0800

    和为K的子数组

---
+++ leetcode560/leetcode560/源.cpp
@@ -0,0 +1,19 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int count = 0;
+        for (int start = 0; start < nums.size(); ++start) {
+            int sum = 0;
+            for (int end = start; end >= 0; --end) {
+                sum += nums[end];
+                if (sum == k) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+};
+
