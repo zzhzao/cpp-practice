a2354fbb1486c7d73f118b951d509eb2582a7309
zzhZAO <3516422381@qq.com>
2025-08-16 20:06:54 +0800

    两整数之和

---
+++ leetcode371/leetcode371/源.cpp
@@ -0,0 +1,13 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+class Solution {
+public:
+    int getSum(int a, int b) {
+        while (b != 0) {
+            unsigned int carry = (unsigned int)(a & b) << 1;
+            a = a ^ b;
+            b = carry;
+        }
+        return a;
+    }
+};
+
