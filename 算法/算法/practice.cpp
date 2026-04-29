f9dbd9e27e3e992f73371b31a129831f1b4b1822
赵泽浩 <3516422381@qq.com>
2026-04-29 11:34:04 +0800

    ⼭峰数组的峰顶

--- 算法/算法/practice.cpp
+++ 算法/算法/practice.cpp
@@ -1,35 +1,20 @@
 class Solution {
 public:
-    vector<int> searchRange(vector<int>& nums, int target) {
-        if (nums.size() == 0)
-        {
-            return { -1,-1 };
-        }
-        int left = 0, right = nums.size() - 1;
-        int resleft = -1;
-        int resright = -1;
+    int peakIndexInMountainArray(vector<int>& arr) {
+        int left = 0, right = arr.size() - 1;
         while (left < right)
         {
-            int mid = left + (right - left) / 2;
-            if (nums[mid] >= target) right = mid;
-            else left = mid + 1;
+            int mid = left + (right - left + 1) / 2;
+            if (arr[mid] < arr[mid - 1])
+            {
+                right = mid - 1;
+            }
+            else
+            {
+                left = mid;
+            }
         }
-        if (nums[left] == target)
-        {
-            resleft = left;
-        }
-        left = 0;right = nums.size() - 1;
-        while (left < right)
-        {
-            int mid = left + (right - left) / 2 + 1;
-            if (nums[mid] <= target) left = mid;
-            else right = mid - 1;
-        }
-        if (nums[right] == target)
-        {
-            resright = right;
-        }
-        return { resleft,resright };
+        return right;
 
     }
 };
