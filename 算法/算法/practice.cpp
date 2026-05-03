f74b175abf8446cae7ee0a0a57b980f14673b371
赵泽浩 <3516422381@qq.com>
2026-05-03 15:08:15 +0800

    二维前缀和

--- 算法/算法/practice.cpp
+++ 算法/算法/practice.cpp
@@ -1,20 +1,31 @@
-class Solution {
-public:
-    int peakIndexInMountainArray(vector<int>& arr) {
-        int left = 0, right = arr.size() - 1;
-        while (left < right)
-        {
-            int mid = left + (right - left + 1) / 2;
-            if (arr[mid] < arr[mid - 1])
-            {
-                right = mid - 1;
-            }
-            else
-            {
-                left = mid;
-            }
-        }
-        return right;
+#include <iostream>
+using namespace std;
 
+const int N = 1010;
+int arr[N][N];
+long long dp[N][N];
+int n, m, q;
+int main() {
+    cin >> n >> m >> q;
+    for (int i = 1;i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            cin >> arr[i][j];
+        }
     }
-};
+    for (int i = 1;i <= n; i++)
+    {
+        for (int j = 1; j <= m; j++)
+        {
+            dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + arr[i][j];
+        }
+    }
+    int x1, x2, y1, y2;
+    while (q--)
+    {
+        cin >> x1 >> y1 >> x2 >> y2;
+        cout << dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1] << endl;
+    }
+
+}
