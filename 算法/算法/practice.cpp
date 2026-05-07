78e189a2c2bf16b16334aaed6f1f5375ae502c51
赵泽浩 <3516422381@qq.com>
2026-05-07 21:19:31 +0800

    连续数组

--- 算法/算法/practice.cpp
+++ 算法/算法/practice.cpp
@@ -1,31 +1,23 @@
-#include <iostream>
-using namespace std;
-
-const int N = 1010;
-int arr[N][N];
-long long dp[N][N];
-int n, m, q;
-int main() {
-    cin >> n >> m >> q;
-    for (int i = 1;i <= n; i++)
-    {
-        for (int j = 1; j <= m; j++)
+class Solution {
+public:
+    int findMaxLength(vector<int>& nums) {
+        unordered_map<int, int> hash;
+        hash[0] = -1;
+        int sum = 0;
+        int ret = 0;
+        for (int i = 0;i < nums.size();i++)
         {
-            cin >> arr[i][j];
-        }
-    }
-    for (int i = 1;i <= n; i++)
-    {
-        for (int j = 1; j <= m; j++)
-        {
-            dp[i][j] = dp[i - 1][j] + dp[i][j - 1] - dp[i - 1][j - 1] + arr[i][j];
-        }
-    }
-    int x1, x2, y1, y2;
-    while (q--)
-    {
-        cin >> x1 >> y1 >> x2 >> y2;
-        cout << dp[x2][y2] - dp[x1 - 1][y2] - dp[x2][y1 - 1] + dp[x1 - 1][y1 - 1] << endl;
-    }
+            sum += nums[i] == 0 ? -1 : 1;
+            if (hash.count(sum))
+            {
+                ret = max(ret, i - hash[sum]);
+            }
+            else
+            {
+                hash[sum] = i;
+            }
 
-}
+        }
+        return ret;
+    }
+};
