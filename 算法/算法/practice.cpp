4aa0c9549baafa83b9e438143e47e08edd0ee128
赵泽浩 <3516422381@qq.com>
2026-05-16 11:50:19 +0800

    快速排序

--- 算法/算法/practice.cpp
+++ 算法/算法/practice.cpp
@@ -1,15 +1,41 @@
 class Solution {
 public:
-    void sortColors(vector<int>& nums) {
-        for (int i = 0;i < nums.size();i++)
+
+
+    void  quicksort(vector<int>& nums, int left, int right)
+    {
+        if (left >= right)
         {
-            for (int j = 0;j < nums.size() - i;j++)
+            return;
+        }
+        int key = getRandom(nums, left, right);
+        int i = left, l = left - 1, r = right + 1;
+        while (i < r)
+        {
+            if (nums[i] < key)
             {
-                if (j != nums.size() - 1 && nums[j] > nums[j + 1])
-                {
-                    swap(nums[j], nums[j + 1]);
-                }
+                swap(nums[i++], nums[++l]);
+            }
+            else if (nums[i] == key)
+            {
+                i++;
+            }
+            else
+            {
+                swap(nums[i], nums[--r]);
             }
         }
+        quicksort(nums, left, l);
+        quicksort(nums, r, right);
+    }
+    int getRandom(vector<int>& nums, int left, int right)
+    {
+        int r = rand();
+        return nums[r % (right - left + 1) + left];
+    }
+    vector<int> sortArray(vector<int>& nums) {
+        srand(time(NULL));
+        quicksort(nums, 0, nums.size() - 1);
+        return nums;
     }
 };
