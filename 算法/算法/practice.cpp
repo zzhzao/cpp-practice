a0524879b78a09f1ee1d607d1dd895c60e4cc651
赵泽浩 <3516422381@qq.com>
2026-05-16 17:36:04 +0800

    反转对

--- 算法/算法/practice.cpp
+++ 算法/算法/practice.cpp
@@ -1,59 +1,51 @@
 class Solution {
-    vector<int> ret = { 0 };
-    vector<int> index;
-    int tmp[500010];
-    int tmpindex[500010];
+    int tmp[50010];
 public:
-    vector<int> countSmaller(vector<int>& nums) {
-        int n = nums.size();
-        ret.resize(n);
-
-        index.resize(n);
-
-        for (int i = 0; i < n; i++)
-            index[i] = i;
-
-        mergeSort(nums, 0, nums.size() - 1);
-        return ret;
+    int reversePairs(vector<int>& nums) {
+        return mergesort(nums, 0, nums.size() - 1);
     }
-    void mergeSort(vector<int>& nums, int left, int right) {
+    int mergesort(vector<int>& nums, int left, int right)
+    {
+        int ret = 0;
         if (left >= right)
-            return;
+        {
+            return 0;
+        }
         int mid = (right - left) / 2 + left;
-
         int cur1 = left, cur2 = mid + 1, i = 0;
-        mergeSort(nums, left, mid);
-        mergeSort(nums, mid + 1, right);
+        ret += mergesort(nums, left, mid);
+        ret += mergesort(nums, mid + 1, right);
 
+        while (cur1 <= mid)
+        {
+            for (;cur2 <= right;cur2++)
+            {
+                if (nums[cur1] / 2.0 > nums[cur2])
+                {
+                    ret += right - cur2 + 1;
+                    break;
+                }
+            }
+            cur1++;
+        }
+
+        cur1 = left, cur2 = mid + 1;
         while (cur1 <= mid && cur2 <= right)
         {
             if (nums[cur1] > nums[cur2])
             {
-                ret[index[cur1]] += right - cur2 + 1;
-                tmpindex[i] = index[cur1];
                 tmp[i++] = nums[cur1++];
             }
             else
             {
-                tmpindex[i] = index[cur2];
                 tmp[i++] = nums[cur2++];
             }
         }
-        while (cur1 <= mid)
-        {
-            tmpindex[i] = index[cur1];
-            tmp[i++] = nums[cur1++];
-        }
-        while (cur2 <= right)
-        {
-            tmpindex[i] = index[cur2];
-            tmp[i++] = nums[cur2++];
-        }
-        for (int j = left;j <= right;j++)
-        {
-            nums[j] = tmp[j - left];
-            index[j] = tmpindex[j - left];
-        }
-    }
+        while (cur1 <= mid)    tmp[i++] = nums[cur1++];
+        while (cur2 <= right)      tmp[i++] = nums[cur2++];
 
+        for (int j = left; j <= right; j++)
+            nums[j] = tmp[j - left];
+        return ret;
+    }
 };
