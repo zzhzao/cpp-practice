6aa301705d78d3e7b65ccec8c715f522910f9f22
赵泽浩 <3516422381@qq.com>
2026-05-16 17:04:13 +0800

    计算右侧小于当前元素的个数

--- 算法/算法/practice.cpp
+++ 算法/算法/practice.cpp
@@ -1,41 +1,59 @@
 class Solution {
+    vector<int> ret = { 0 };
+    vector<int> index;
+    int tmp[500010];
+    int tmpindex[500010];
 public:
+    vector<int> countSmaller(vector<int>& nums) {
+        int n = nums.size();
+        ret.resize(n);
 
+        index.resize(n);
 
-    void  quicksort(vector<int>& nums, int left, int right)
-    {
+        for (int i = 0; i < n; i++)
+            index[i] = i;
+
+        mergeSort(nums, 0, nums.size() - 1);
+        return ret;
+    }
+    void mergeSort(vector<int>& nums, int left, int right) {
         if (left >= right)
-        {
             return;
-        }
-        int key = getRandom(nums, left, right);
-        int i = left, l = left - 1, r = right + 1;
-        while (i < r)
+        int mid = (right - left) / 2 + left;
+
+        int cur1 = left, cur2 = mid + 1, i = 0;
+        mergeSort(nums, left, mid);
+        mergeSort(nums, mid + 1, right);
+
+        while (cur1 <= mid && cur2 <= right)
         {
-            if (nums[i] < key)
+            if (nums[cur1] > nums[cur2])
             {
-                swap(nums[i++], nums[++l]);
-            }
-            else if (nums[i] == key)
-            {
-                i++;
+                ret[index[cur1]] += right - cur2 + 1;
+                tmpindex[i] = index[cur1];
+                tmp[i++] = nums[cur1++];
             }
             else
             {
-                swap(nums[i], nums[--r]);
+                tmpindex[i] = index[cur2];
+                tmp[i++] = nums[cur2++];
             }
         }
-        quicksort(nums, left, l);
-        quicksort(nums, r, right);
+        while (cur1 <= mid)
+        {
+            tmpindex[i] = index[cur1];
+            tmp[i++] = nums[cur1++];
+        }
+        while (cur2 <= right)
+        {
+            tmpindex[i] = index[cur2];
+            tmp[i++] = nums[cur2++];
+        }
+        for (int j = left;j <= right;j++)
+        {
+            nums[j] = tmp[j - left];
+            index[j] = tmpindex[j - left];
+        }
     }
-    int getRandom(vector<int>& nums, int left, int right)
-    {
-        int r = rand();
-        return nums[r % (right - left + 1) + left];
-    }
-    vector<int> sortArray(vector<int>& nums) {
-        srand(time(NULL));
-        quicksort(nums, 0, nums.size() - 1);
-        return nums;
-    }
+
 };
