abbc977791c1d6c6046f7c14a2be7832e208d63e
赵泽浩 <3516422381@qq.com>
2026-05-18 15:53:45 +0800

    重排链表

--- 算法/算法/practice.cpp
+++ 算法/算法/practice.cpp
@@ -1,30 +1,53 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
 class Solution {
 public:
-    ListNode* swapPairs(ListNode* head) {
-        ListNode* Cur = head;
-        if (head == nullptr)
+    void reorderList(ListNode* head) {
+        if (head == nullptr || head->next == nullptr || head->next->next == nullptr)
         {
-            return head;
+            return;
         }
-        ListNode* Next = head->next;
-        if (Next == nullptr)
+        ListNode* slow = head, * fast = head;
+        while (fast && fast->next)
         {
-            return head;
+            slow = slow->next;
+            fast = fast->next->next;
         }
-        ListNode* ret = Next;
-        ListNode* prev = nullptr;
-        while (Cur && Next)
+        ListNode* head2 = new ListNode(0);
+        ListNode* cur = slow->next;
+        slow->next = nullptr;
+        ListNode* next = nullptr;
+        while (cur)
         {
-            Cur->next = Next->next;
-            Next->next = Cur;
-            if (prev != nullptr)
-                prev->next = Next;
-            prev = Cur;
+            next = cur->next;
+            cur->next = head2->next;
+            head2->next = cur;
+            cur = next;
+        }
 
-            Cur = Cur->next;
-            if (Cur != nullptr)
-                Next = Cur->next;
+        ListNode* cur1 = head, * cur2 = head2->next;
+        ListNode* next1 = nullptr;
+        ListNode* next2 = nullptr;
+
+        while (cur1 && cur2)
+        {
+            next1 = cur1->next;
+            cur1->next = cur2;
+            next2 = cur2->next;
+            cur2->next = next1;
+            cur1 = next1;
+
+            cur2 = next2;
         }
-        return ret;
+
+        delete head2;
     }
 };
