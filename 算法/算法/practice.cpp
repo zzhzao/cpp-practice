5a9572215ee1bdec77c037dd9394b13ad371279d
赵泽浩 <3516422381@qq.com>
2026-05-18 16:58:16 +0800

    合并K个升序链表

--- 算法/算法/practice.cpp
+++ 算法/算法/practice.cpp
@@ -9,45 +9,36 @@
  * };
  */
 class Solution {
+    struct cmp
+    {
+        bool operator()(const ListNode* l1, const ListNode* l2)
+        {
+            return l1->val > l2->val;
+        }
+    };
+
 public:
-    void reorderList(ListNode* head) {
-        if (head == nullptr || head->next == nullptr || head->next->next == nullptr)
-        {
-            return;
-        }
-        ListNode* slow = head, * fast = head;
-        while (fast && fast->next)
-        {
-            slow = slow->next;
-            fast = fast->next->next;
-        }
-        ListNode* head2 = new ListNode(0);
-        ListNode* cur = slow->next;
-        slow->next = nullptr;
-        ListNode* next = nullptr;
-        while (cur)
-        {
-            next = cur->next;
-            cur->next = head2->next;
-            head2->next = cur;
-            cur = next;
-        }
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        priority_queue<ListNode*, vector<ListNode*>, cmp> heap;
 
-        ListNode* cur1 = head, * cur2 = head2->next;
-        ListNode* next1 = nullptr;
-        ListNode* next2 = nullptr;
-
-        while (cur1 && cur2)
+        for (auto e : lists)
         {
-            next1 = cur1->next;
-            cur1->next = cur2;
-            next2 = cur2->next;
-            cur2->next = next1;
-            cur1 = next1;
-
-            cur2 = next2;
+            if (e)
+                heap.push(e);
         }
-
-        delete head2;
+        ListNode* head = new ListNode(0);
+        ListNode* prev = head;
+        while (!heap.empty())
+        {
+            ListNode* top = heap.top();
+            heap.pop();
+            prev->next = top;
+            prev = top;
+            if (top->next)
+                heap.push(top->next);
+        }
+        prev = head->next;
+        delete head;
+        return prev;
     }
 };
