5e697c7a12c3be67d972867f38de4334b9ed9196
赵泽浩 <3516422381@qq.com>
2026-05-18 14:58:17 +0800

    两两交换链表中的节点

--- 算法/算法/practice.cpp
+++ 算法/算法/practice.cpp
@@ -1,31 +1,30 @@
 class Solution {
 public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        ListNode* ret = new ListNode(0);
-        ListNode* Ret = ret;
-        int cnt = 0;
-        while (l1 || l2 || cnt)
+    ListNode* swapPairs(ListNode* head) {
+        ListNode* Cur = head;
+        if (head == nullptr)
         {
-            int tmp = 0;
-            if (l1)
-            {
-                tmp += l1->val;
-                l1 = l1->next;
-            }
-            if (l2)
-            {
-                tmp += l2->val;
-                l2 = l2->next;
-            }
-            tmp += cnt;
-            cnt = 0;
-            if (tmp > 9)
-            {
-                cnt = tmp / 10;
-            }
-            ret->next = new ListNode(tmp % 10);
-            ret = ret->next;
+            return head;
         }
-        return Ret->next;
+        ListNode* Next = head->next;
+        if (Next == nullptr)
+        {
+            return head;
+        }
+        ListNode* ret = Next;
+        ListNode* prev = nullptr;
+        while (Cur && Next)
+        {
+            Cur->next = Next->next;
+            Next->next = Cur;
+            if (prev != nullptr)
+                prev->next = Next;
+            prev = Cur;
+
+            Cur = Cur->next;
+            if (Cur != nullptr)
+                Next = Cur->next;
+        }
+        return ret;
     }
 };
