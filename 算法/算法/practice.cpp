300463ed735c531046ab18393951dda4df542b61
赵泽浩 <3516422381@qq.com>
2026-05-19 10:09:34 +0800

    两数之和

--- 算法/算法/practice.cpp
+++ 算法/算法/practice.cpp
@@ -1,44 +1,19 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
-    struct cmp
-    {
-        bool operator()(const ListNode* l1, const ListNode* l2)
-        {
-            return l1->val > l2->val;
-        }
-    };
-
 public:
-    ListNode* mergeKLists(vector<ListNode*>& lists) {
-        priority_queue<ListNode*, vector<ListNode*>, cmp> heap;
-
-        for (auto e : lists)
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> hash;
+        for (int i = 0;i < nums.size();i++)
         {
-            if (e)
-                heap.push(e);
+            auto it = hash.find(target - nums[i]);
+            if (it != hash.end())
+            {
+                return { it->second,i };
+            }
+            else
+            {
+                hash[nums[i]] = i;
+            }
         }
-        ListNode* head = new ListNode(0);
-        ListNode* prev = head;
-        while (!heap.empty())
-        {
-            ListNode* top = heap.top();
-            heap.pop();
-            prev->next = top;
-            prev = top;
-            if (top->next)
-                heap.push(top->next);
-        }
-        prev = head->next;
-        delete head;
-        return prev;
+        return { -1,-1 };
     }
 };
