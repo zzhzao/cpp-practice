cdb4c46c0f262d66ad9bd5e44b985575514eb4cd
赵泽浩 <3516422381@qq.com>
2026-05-20 09:41:43 +0800

    二进制求和

--- 算法/算法/practice.cpp
+++ 算法/算法/practice.cpp
@@ -1,35 +1,26 @@
 class Solution {
 public:
-    string longestPalindrome(string s) {
-        int n = s.size();
-        int begin = 0, len = 0;
-        for (int i = 0;i < n;i++)
+    string addBinary(string a, string b) {
+        int cur1 = a.size() - 1;
+        int cur2 = b.size() - 1;
+        int count = 0;
+        string ret;
+        while (cur1 >= 0 || cur2 >= 0 || count > 0)
         {
-            int left = i;
-            int right = i;
-            while (left >= 0 && right < n && s[left] == s[right])
+            if (cur1 >= 0) count += a[cur1--] - '0';
+            if (cur2 >= 0) count += b[cur2--] - '0';
+
+            if (count % 2 == 0)
             {
-                left--;
-                right++;
+                ret += '0';
             }
-            if (right - left - 1 > len)
+            else
             {
-                begin = left + 1;
-                len = right - left - 1;
+                ret += '1';
             }
-            left = i;
-            right = i + 1;
-            while (left >= 0 && right < n && s[left] == s[right])
-            {
-                left--;
-                right++;
-            }
-            if (right - left - 1 > len)
-            {
-                begin = left + 1;
-                len = right - left - 1;
-            }
+            count /= 2;
         }
-        return s.substr(begin, len);
+        reverse(ret.begin(), ret.end());
+        return ret;
     }
 };
