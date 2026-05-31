1ad2b0cf8c30515f304ac46cac17b73331dd9c9f
赵泽浩 <3516422381@qq.com>
2026-05-31 10:58:56 +0800

    比较含退格的字符串

--- 算法/算法/practice.cpp
+++ 算法/算法/practice.cpp
@@ -1,18 +1,34 @@
 class Solution {
 public:
-        string removeDuplicates(string s) {
-            string ret;
-            for (auto e : s)
+    bool backspaceCompare(string s, string t) {
+        string s1, s2;
+        for (auto e : s)
+        {
+            if (!s1.empty() && e == '#')
             {
-                if (!ret.empty() && e == ret.back())
-                {
-                    ret.pop_back();
-                }
-                else
-                {
-                    ret.push_back(e);
-                }
+                s1.pop_back();
             }
-            return ret;
+            else if (e != '#')
+            {
+                s1.push_back(e);
+            }
         }
-    };
+        for (auto e : t)
+        {
+            if (!s2.empty() && e == '#')
+            {
+                s2.pop_back();
+            }
+            else if (e != '#')
+            {
+                s2.push_back(e);
+            }
+        }
+        if (s1 == s2)
+        {
+            return true;
+        }
+        return false;
+
+    }
+};
