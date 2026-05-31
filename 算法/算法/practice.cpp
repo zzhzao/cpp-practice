f0b20e3447357db8d2d385e5bc7d720363ddec52
赵泽浩 <3516422381@qq.com>
2026-05-31 10:47:54 +0800

    删除字符串中所有相邻的重复项

--- 算法/算法/practice.cpp
+++ 算法/算法/practice.cpp
@@ -1,30 +1,18 @@
 class Solution {
 public:
-    string multiply(string num1, string num2) {
-        int m = num1.size(), n = num2.size();
-        reverse(num1.begin(), num1.end());
-        reverse(num2.begin(), num2.end());
-        vector<int> tmp(m + n - 1);
-
-        for (int i = 0;i < m;i++)
-        {
-            for (int j = 0; j < n;j++)
+        string removeDuplicates(string s) {
+            string ret;
+            for (auto e : s)
             {
-                tmp[i + j] += (num1[i] - '0') * (num2[j] - '0');
+                if (!ret.empty() && e == ret.back())
+                {
+                    ret.pop_back();
+                }
+                else
+                {
+                    ret.push_back(e);
+                }
             }
+            return ret;
         }
-        string ret;
-        int count = 0;
-        int cur = 0;
-        while (cur < m + n - 1 || count)
-        {
-            if (cur < m + n - 1)
-                count += tmp[cur++];
-            ret += (count % 10) + '0';
-            count /= 10;
-        }
-        while (ret.size() > 1 && ret.back() == '0') ret.pop_back();
-        reverse(ret.begin(), ret.end());
-        return ret;
-    }
-};
+    };
