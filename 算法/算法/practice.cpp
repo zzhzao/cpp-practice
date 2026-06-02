0e3dbd73581c739d76af7df80fb81f3be77ebd45
赵泽浩 <3516422381@qq.com>
2026-06-02 13:29:55 +0800

    验证栈序列

--- 算法/算法/practice.cpp
+++ 算法/算法/practice.cpp
@@ -1,57 +1,21 @@
 class Solution {
 public:
-    string decodeString(string s) {
-        vector<int> nums;
-        vector<string> strs;
-        int i = 0;
-        int n = s.size();
-        while (i < n)
+    bool validateStackSequences(vector<int>& pushed, vector<int>& popped) {
+        stack<int> st;
+        int i = 0, n = popped.size();
+        for (auto e : pushed)
         {
-            if (s[i] >= '0' && s[i] <= '9')
+            st.push(e);
+            while (st.size() && st.top() == popped[i])
             {
-                int tmp = 0;
-                while (i < n && s[i] >= '0' && s[i] <= '9')
-                {
-                    tmp = tmp * 10 + (s[i] - '0');
-                    i++;
-                }
-                nums.push_back(tmp);
-            }
-            else if (s[i] == '[')
-            {
-                strs.push_back("");
+                st.pop();
                 i++;
-
-            }
-            else if (s[i] == ']')
-            {
-                int k = nums.back();
-                nums.pop_back();
-                string tmp;
-                while (k--)
-                {
-                    tmp += strs.back();
-                }
-                strs.pop_back();
-                if (!strs.empty()) {
-                    strs.back() += tmp;
-                }
-                else {
-                    strs.push_back(tmp);
-                }
-                i++;
-            }
-            else
-            {
-                string str;
-                while (i < n && s[i] >= 'a' && s[i] <= 'z')
-                {
-                    str += s[i++];
-                }
-                if (!strs.empty()) strs.back() += str;
-                else strs.push_back(str);
             }
         }
-        return strs[0];
+        if (i == n)
+        {
+            return true;
+        }
+        return false;
     }
 };
