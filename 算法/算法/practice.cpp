03849d68c5a3f02b75337092fafa840bbbe65b8d
赵泽浩 <3516422381@qq.com>
2026-06-03 09:49:54 +0800

    二叉树的锯齿形层序遍历

--- 算法/算法/practice.cpp
+++ 算法/算法/practice.cpp
@@ -1,29 +1,38 @@
-
 class Solution {
 public:
-    vector<vector<int>> levelOrder(Node* root) {
+    vector<vector<int>> zigzagLevelOrder(TreeNode* root) {
         vector<vector<int>> ret;
-        queue<Node*> q;
+        queue<TreeNode*> q;
         if (root == nullptr)
         {
             return ret;
         }
         q.push(root);
+        int level = 0;
         while (q.size())
         {
+
             int sz = q.size();
             vector<int> tmp;
             for (int i = 0; i < sz;i++)
             {
-                Node* node = q.front();
+                TreeNode* node = q.front();
                 tmp.push_back(node->val);
-                for (Node* e : node->children)
+                if (node->left)
                 {
-                    if (e != nullptr)
-                        q.push(e);
+                    q.push(node->left);
+                }
+                if (node->right)
+                {
+                    q.push(node->right);
                 }
                 q.pop();
             }
+            if (level % 2 == 1)
+            {
+                reverse(tmp.begin(), tmp.end());
+            }
+            level++;
             ret.push_back(tmp);
         }
         return ret;
