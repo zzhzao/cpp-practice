790429cc50a33f70ccbe1b8d27f1ed2f4c39efba
赵泽浩 <3516422381@qq.com>
2026-06-08 16:02:50 +0800

    迷宫中离入口最近的出口

--- 算法/算法/practice.cpp
+++ 算法/算法/practice.cpp
@@ -1,47 +1,39 @@
 class Solution {
     int dx[4] = { 0,0,1,-1 };
     int dy[4] = { 1,-1,0,0 };
-    bool vis[301][301];
-    int m, n;
 public:
-    void solve(vector<vector<char>>& board) {
-
-        m = board.size();
-        n = board[0].size();
-        vector<vector<char>> ret(m, vector<char>(n, 'X'));
-        for (int i = 0;i < m;i++)
-        {
-            for (int j = 0;j < n;j++)
-            {
-                if ((i == 0 || j == 0 || i == m - 1 || j == n - 1) && (board[i][j] == 'O'))
-                {
-                    bfs(board, ret, i, j);
-                }
-            }
-        }
-        board = ret;
-    }
-    void bfs(vector<vector<char>>& board, vector<vector<char>>& ret, int i, int j)
-    {
+    int nearestExit(vector<vector<char>>& maze, vector<int>& entrance) {
+        int m = maze.size(), n = maze[0].size();
+        bool vis[m][n];
+        memset(vis, 0, sizeof(vis));
         queue<pair<int, int>> q;
-        q.push({ i,j });
-        ret[i][j] = 'O';
-        vis[i][j] = true;
+        q.push({ entrance[0],entrance[1] });
+        vis[entrance[0]][entrance[1]] = true;
+        int step = 0;
         while (q.size())
         {
-            auto [a, b] = q.front();
-            q.pop();
-            for (int k = 0; k < 4;k++)
+            step++;
+            int sz = q.size();
+            for (int j = 0; j < sz;j++)
             {
-                int x = a + dx[k];
-                int y = b + dy[k];
-                if (x >= 0 && x < m && y >= 0 && y < n && board[x][y] == 'O' && !vis[x][y])
+                auto [a, b] = q.front();
+                q.pop();
+                for (int i = 0;i < 4;i++)
                 {
-                    q.push({ x,y });
-                    ret[x][y] = 'O';
-                    vis[x][y] = true;
+                    int x = a + dx[i];
+                    int y = b + dy[i];
+                    if (x >= 0 && x < m && y >= 0 && y < n && maze[x][y] == '.' && !vis[x][y])
+                    {
+                        q.push({ x,y });
+                        vis[x][y] = true;
+                        if (x == 0 || x == m - 1 || y == 0 || y == n - 1)
+                        {
+                            return step;
+                        }
+                    }
                 }
             }
         }
+        return -1;
     }
 };
